0c0e25e3f6996484b847e208281e3033bf4f90a1
MaxtecĦ <kevinjonathan4976@gmail.com>
2026-06-18 15:06:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -356,3 +356,22 @@ function transferMoney() {
 function logout() {
     window.location.href = "login.html";
 }
+function toggleDarkMode(){
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+localStorage.setItem("theme","dark");
+}else{
+localStorage.setItem("theme","light");
+}
+
+}
+
+window.onload=function(){
+
+if(localStorage.getItem("theme")==="dark"){
+document.body.classList.add("dark-mode");
+}
+
+};
